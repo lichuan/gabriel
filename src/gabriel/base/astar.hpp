ac872b4f8247b828ce4e6d8edc2ee65b68732d2c
lichuan <308831759@qq.com>
2013-11-28 21:33:03 +0800

    add yasnippet

--- src/gabriel/base/astar.hpp
+++ src/gabriel/base/astar.hpp
@@ -1,12 +1,23 @@
-/*
- author: lichuan
- qq: 308831759
- email: 308831759@qq.com
- homepage: www.lichuan.me
- github: https://github.com/lichuan/gabriel
- date: 2013-08-23
- desc: a星寻路的一种高效实现
-*/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *      _____       ___   _____   _____    _   _____   _               *
+ *     /  ___|     /   | |  _  \ |  _  \  | | | ____| | |              *
+ *     | |        / /| | | |_| | | |_| |  | | | |__   | |              *
+ *     | |  _    / / | | |  _  { |  _  /  | | |  __|  | |              *
+ *     | |_| |  / /  | | | |_| | | | \ \  | | | |___  | |___           *
+ *     \_____/ /_/   |_| |_____/ |_|  \_\ |_| |_____| |_____|          *
+ *                                                                     *
+ *     gabriel is an angel from the Holy Bible, this engine is         *
+ *   named gabriel, standing for bring good news to people. the        *
+ *   goal of gabriel server engine is to help people to develop        *
+ *   various online games, welcome you to join in.                     *
+ *                                                                     *
+ *   @author: lichuan                                                  *
+ *   @qq: 308831759                                                    *
+ *   @email: 308831759@qq.com                                          *
+ *   @github: https://github.com/lichuan/gabriel                       *
+ *   @date: 2013-11-28 21:25:31                                        *
+ *                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef GABRIEL__BASE__ASTAR
 #define GABRIEL__BASE__ASTAR
--- src/gabriel/base/server_connection.hpp
+++ src/gabriel/base/server_connection.hpp
@@ -1,3 +1,24 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *      _____       ___   _____   _____    _   _____   _               *
+ *     /  ___|     /   | |  _  \ |  _  \  | | | ____| | |              *
+ *     | |        / /| | | |_| | | |_| |  | | | |__   | |              *
+ *     | |  _    / / | | |  _  { |  _  /  | | |  __|  | |              *
+ *     | |_| |  / /  | | | |_| | | | \ \  | | | |___  | |___           *
+ *     \_____/ /_/   |_| |_____/ |_|  \_\ |_| |_____| |_____|          *
+ *                                                                     *
+ *     gabriel is an angel from the Holy Bible, this engine is         *
+ *   named gabriel, standing for bring good news to people. the        *
+ *   goal of gabriel server engine is to help people to develop        *
+ *   various online games, welcome you to join in.                     *
+ *                                                                     *
+ *   @author: lichuan                                                  *
+ *   @qq: 308831759                                                    *
+ *   @email: 308831759@qq.com                                          *
+ *   @github: https://github.com/lichuan/gabriel                       *
+ *   @date: 2013-11-28 21:29:34                                        *
+ *                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef GABRIEL__BASE__SERVER_CONNECTION
 #define GABRIEL__BASE__SERVER_CONNECTION
 
