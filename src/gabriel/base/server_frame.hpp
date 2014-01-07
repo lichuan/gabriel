d81ae0d2bd3bf40a62256661690e5049a2d580a4
lichuan <308831759@qq.com>
2014-01-07 09:02:21 +0800

    add frame

---
+++ src/gabriel/base/server_frame.hpp
@@ -0,0 +1,48 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *      _____       ___   _____   _____    _   _____   _               *
+ *     /  ___|     /   | |  _  \ |  _  \  | | | ____| | |              *
+ *     | |        / /| | | |_| | | |_| |  | | | |__   | |              *
+ *     | |  _    / / | | |  _  { |  _  /  | | |  __|  | |              *
+ *     | |_| |  / /  | | | |_| | | | \ \  | | | |___  | |___           *
+ *     \_____/ /_/   |_| |_____/ |_|  \_\ |_| |_____| |_____|          *
+ *                                                                     *
+ *     gabriel is an angel from the Holy Bible, this engine is named   *
+ *   gabriel, means bringing people good news. the goal of gabriel     *
+ *   server engine is to help people to develop various online games,  *
+ *   welcome you to join in.                                           *
+ *                                                                     *
+ *   @author: lichuan                                                  *
+ *   @qq: 308831759                                                    *
+ *   @email: 308831759@qq.com                                          *
+ *   @site: www.lichuan.me                                             *
+ *   @github: https://github.com/lichuan/gabriel                       *
+ *   @date: 2014-01-07 08:55:58                                        *
+ *                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef GABRIEL__BASE__SERVER_FRAME
+#define GABRIEL__BASE__SERVER_FRAME
+
+namespace gabriel {
+namespace base {
+
+template<typename T>
+class Server_Frame
+{
+public:
+    Server_Frame()
+    {
+        T::instance()->init();
+        T::instance()->run();        
+    }
+
+    ~Server_Frame()
+    {
+        T::instance()->fini();
+    }    
+};
+
+}
+}
+
+#endif
