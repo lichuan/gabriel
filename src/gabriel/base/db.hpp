5df2e064d848b25924d8b59ada52a595009cea56
lichuan <308831759@qq.com>
2014-03-23 23:04:27 +0800

    md proto

---
+++ src/gabriel/base/db.hpp
@@ -0,0 +1,39 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *      _____       ___   _____   _____    _   _____   _               *
+ *     /  ___|     /   | |  _  \ |  _  \  | | | ____| | |              *
+ *     | |        / /| | | |_| | | |_| |  | | | |__   | |              *
+ *     | |  _    / / | | |  _  { |  _  /  | | |  __|  | |              *
+ *     | |_| |  / /  | | | |_| | | | \ \  | | | |___  | |___           *
+ *     \_____/ /_/   |_| |_____/ |_|  \_\ |_| |_____| |_____|          *
+ *                                                                     *
+ *     gabriel is an angel from the Holy Bible, this engine is named   *
+ *   gabriel, means bringing people good news. the goal of gabriel     *
+ *   server engine is to help people to develop various online games,  *
+ *   welcome you to join in.                                           *
+ *                                                                     *
+ *   @author: lichuan                                                  *
+ *   @qq: 308831759                                                    *
+ *   @email: 308831759@qq.com                                          *
+ *   @site: www.lichuan.me                                             *
+ *   @github: https://github.com/lichuan/gabriel                       *
+ *   @date: 2014-03-23 20:10:05                                        *
+ *                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef GABRIEL__BASE__DB
+#define GABRIEL__BASE__DB
+
+namespace gabriel {
+namespace base {
+
+class DBConn
+{
+public:
+    DBConn();
+    ~DBConn();
+};
+    
+}
+}
+        
+#endif
