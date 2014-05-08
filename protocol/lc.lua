0f099c5ba608b8e6c2c737c7f78fb9434384b9c0
lichuan <308831759@qq.com>
2014-05-09 00:25:40 +0800

    md lua testcase

--- protocol/lc.lua
+++ protocol/lc.lua
@@ -3,10 +3,25 @@
 package.cpath = "./protobuf/?.so"
 package.path = "./generated/?.lua;./protobuf/?.lua;"
 require("gabriel.protocol.server.db_pb")
-local dd = gabriel.protocol.server.db_pb
-print(dd)
-local msg = dd.DB_Context()
-msg.seq = 999999
-msg.client_id =2323
+local db = gabriel.protocol.server.db_pb
+local msg = db.DB_Msg()
+msg.dbmsg_id = 98
+msg.monster.monster_id = 22
+msg.monster.monster_hp =92
+msg.monster.monster_name = "a huge monster"
+msg.outerenum = db.ENUM_OUTER_1
+msg.outer.outer_id = 3332
+msg.outer.outer_string = "outer string"
+msg.innerenum = msg.outer.ENUM_INNER_1
+msg.inner.inner_id = 892
+msg.inner.inner_string = "inner string"
+msg.pubmsg.pubid = 232
+msg.pubmsg.pubname = "pub name"
+msg.pubenum = gabriel.protocol.shared_pb.ENUMPUB_1
+msg.pubouter.outerid = 1112
+msg.pubouter.outername = "outername"
+msg.pubinner.innerid = 29323
+msg.pubinner.innername = "innername"
+msg.pubinnerenum = msg.pubouter.ENUM_PUBINNER_2
 print(msg)
 
